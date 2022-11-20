c55f1e123cde850d3d1c2a33c9116c2b83a9133e
keithchasen <keithchasen89@gmail.com>
2022-11-20 21:47:53 +0200

    basic set up apollo

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+    ApolloClient,
+    createHttpLink,
+    ApolloLink,
+    InMemoryCache,
+    ApolloProvider
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+    uri: process.env.FE_URL,
+    credentials: 'include'
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = '';
+    return {
+        headers: {
+            ...headers,
+            authorization: `Bearer ${token}`
+        }
+    }
+});
+
+const client = new ApolloClient({
+    link: ApolloLink.from([authLink, httpLink]),
+    cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={client}>
+          <App />
+      </ApolloProvider>
   </React.StrictMode>
 );
