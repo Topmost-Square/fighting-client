f99644fecd8f7545b8d7a59a24a91a161aceee86
keithchasen <keithchasen89@gmail.com>
2022-11-21 18:23:00 +0200

    added tailwind / basic markup for login/ register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from 'react-router-dom';
+import {Register} from "./pages/Register";
+import {Login} from "./pages/Login";
+import {Games} from "./pages/Games";
 
 function App() {
   return (
-    <div className="App">
-      game center
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path='register' element={ <Register />}/>
+            <Route path='login' element={ <Login />}/>
+            <Route path='/' element={ <Games />}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
