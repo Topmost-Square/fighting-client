1763ab09d5ad0e386179fd129e9278d6564bc8a7
keithchasen <keithchasen89@gmail.com>
2022-11-29 23:01:01 +0200

    logout button

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,11 +1,19 @@
 type MenuItemProps = {
     name: string,
-    top?: boolean
+    top?: boolean,
+    click?: () => void
 }
 
-export const MenuItem = ({ name, top }: MenuItemProps) => {
+export const MenuItem = ({ name, top, click }: MenuItemProps) => {
     const topStyles = top ? 'mt-10' : '';
     const styles = 'w-1/2 h-1/6 bg-violet-300 mb-10 flex flex-col items-center justify-center';
     const hoverStyles = 'hover:bg-violet-500 hover:drop-shadow-2xl';
-    return <div className={[topStyles, styles, hoverStyles].join(' ')}>{name}</div>
+    return (
+        <div
+            className={[topStyles, styles, hoverStyles].join(' ')}
+            onClick={click ? () => click() : () => {}}
+        >
+            {name}
+        </div>
+    );
 }
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,13 +1,19 @@
 import {MenuItem} from "../components/MenuItem";
+import {removeToken} from "../utils/auth";
 
 export const Menu = () => {
+    const exit = () => {
+        removeToken();
+        document.location.reload();
+    }
+
     return (
         <div className='flex flex-col items-center h-screen'>
             <MenuItem name='Practice' top={true} />
             <MenuItem name='Online' />
             <MenuItem name='Inventory' />
             <MenuItem name='Settings' />
-            <MenuItem name='Exit' />
+            <MenuItem name='Exit' click={exit}/>
         </div>
     );
 }
